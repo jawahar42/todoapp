911b2cc84db47211ad37d9e157df16cd66659272
Jawahar Reddy <jawahar.reddy007@gmail.com>
2018-08-10 17:48:09 +0530

    Add TaskModel to models.py

--- todo/models.py
+++ todo/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class TaskModel(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=200, null=True, default=None)
     due_date = models.DateTimeField()
     parent_task = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
