4f8c45e28218860aaa09303333ac2d030a78c733
Jawahar Reddy <jawahar.reddy007@gmail.com>
2018-08-10 17:48:57 +0530

    Add a simple view
    Shows tasks with approaching deadlines.

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TaskModel
+from django.utils import timezone
 
 def index(request):
     return HttpResponse("Hello, world. You're at the todo index.")
+
+def upcoming(request):
+    upcoming_task_list = TaskModel.objects.filter(due_date__gte=timezone.now().strftime("%Y-%m-%d %H:%M:%S"))[:5]
+    output = ', '.join([t.title for t in upcoming_task_list])
+    return HttpResponse(output)
